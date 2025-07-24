460a243b919f8ca8b36e7ff0fc4db19ffd713669
prasad7041 <chitti0718@gmail.com>
2025-07-24 23:57:52 +0530

    Update intro.js

--- intro/intro.js
+++ intro/intro.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://lynk-wdbc.onrender.com',
   credentials: true
 }));
 app.use('/user', route);
